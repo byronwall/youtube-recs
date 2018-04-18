18d6583fe23851be7ac41195be89dfb46489a1b6
Byron Wall <byron@byroni.us>
2018-04-17 22:39:21 -0400

    Correct static address for production

--- server.ts
+++ server.ts
@@ -329,7 +329,7 @@ app.get("/data", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const port = process.env.PORT || 3001;
